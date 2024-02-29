19a1a076952a802bc0917c910146fbace71ac78a
mario <codemario318@gmail.com>
2024-02-29 21:49:48 +0900

    [FIX] 서비스 로직 리펙토링

--- src/main/kotlin/com/mario/websocket/user/UserService.kt
+++ src/main/kotlin/com/mario/websocket/user/UserService.kt
@@ -1,29 +1,21 @@
 package com.mario.websocket.user
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.lang.IllegalStateException
 
 @Service
 class UserService(
     private val repository: UserRepository,
 ) {
     fun saveUser(user: User) {
-        repository.save(User(
-            nickName = user.nickName,
-            fullName = user.fullName,
-            status = Status.ONLINE,
-        ))
+        repository.save(user.copy(status = Status.ONLINE))
     }
 
     fun disconnect(user: User) {
-        val storedUser = repository.findById(user.nickName).orElse(null)
-
-        if (storedUser != null) {
-            repository.save(User(
-                nickName = user.nickName,
-                fullName = user.fullName,
-                status = Status.OFFLINE,
-            ))
-        }
+        repository.findByIdOrNull(user.nickName)?.let { storedUser ->
+            repository.save(storedUser.copy(status = Status.OFFLINE))
+        } ?: throw IllegalStateException("잘못된 요청입니다.")
     }
 
     fun findConnectedUsers(): List<User> {
